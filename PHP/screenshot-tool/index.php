98de6ccde13d8d8c4ff71f5dffea919497597f48
Hugo Paz <dominuswebs@gmail.com>
2026-02-14 13:58:13 +1100

    added PDF generation for screenshot tool

--- PHP/screenshot-tool/index.php
+++ PHP/screenshot-tool/index.php
@@ -1,46 +1,58 @@
 <?php
+ini_set('max_execution_time', 300);
+
 
 require_once 'vendor/autoload.php';
 
 use HeadlessChromium\BrowserFactory;
 use HeadlessChromium\Page;
+use Mpdf\Mpdf;
 
-$browserFactory = new BrowserFactory();
+// $browserFactory = new BrowserFactory();
 
-// starts headless Chrome
-$browser = $browserFactory->createBrowser(
-    ['windowSize' => [1920, 1080],]
-);
+// // starts headless Chrome
+// $browser = $browserFactory->createBrowser(
+//     ['windowSize' => [1920, 1080],]
+// );
 
-try {
-    // creates a new page and navigate to an URL
-    $page = $browser->createPage();
+// try {
+//     // creates a new page and navigate to an URL
+//     $page = $browser->createPage();
 
-    $urls = [
-        'http://localhost/Work/DIT/DDMB-Framework/P4%20Dev/content/P12_D_4PL1/?disableanimation=true&tags=[hide_burger_section,mall_condition]',
-        'http://localhost/Work/DIT/DDMB-Framework/P4%20Dev/content/P12_D_4PL2/?disableanimation=true&tags=[hide_burger_section,mall_condition]',
-        'http://localhost/Work/DIT/DDMB-Framework/P4%20Dev/content/P12_D_4PL3/?disableanimation=true&tags=[hide_burger_section,mall_condition]',
-        'http://localhost/Work/DIT/DDMB-Framework/P4%20Dev/content/P12_D_4PL4/?disableanimation=true&tags=[hide_burger_section,mall_condition]'
-    ];
+//     $urls = [
+//         'http://localhost/Work/DIT/DDMB-Framework/P4%20Dev/content/P12_D_4PL1/?disableanimation=true&tags=[hide_burger_section,mall_condition]',
+//         'http://localhost/Work/DIT/DDMB-Framework/P4%20Dev/content/P12_D_4PL2/?disableanimation=true&tags=[hide_burger_section,mall_condition]',
+//         'http://localhost/Work/DIT/DDMB-Framework/P4%20Dev/content/P12_D_4PL3/?disableanimation=true&tags=[hide_burger_section,mall_condition]',
+//         'http://localhost/Work/DIT/DDMB-Framework/P4%20Dev/content/P12_D_4PL4/?disableanimation=true&tags=[hide_burger_section,mall_condition]'
+//     ];
 
-    for($i = 0; $i < count($urls); $i++) {
-        $page->navigate($urls[$i])->waitForNavigation(Page::NETWORK_IDLE);
-        // get page title
-        $pageTitle = $page->evaluate('document.title')->getReturnValue();
-        // screenshot - Say "Cheese"! 😄
-        sleep(3);
-        $page->screenshot([
-            'captureBeyondViewport' => true,
-            'clip' => $page->getFullPageClip()
-        ])->saveToFile(__DIR__ . "/screenshots/bar{$i}.png");
-    }
+//     for($i = 0; $i < count($urls); $i++) {
+//         $page->navigate($urls[$i])->waitForNavigation(Page::NETWORK_IDLE);
+//         // get page title
+//         $pageTitle = $page->evaluate('document.title')->getReturnValue();
+//         // screenshot - Say "Cheese"! 😄
+//         sleep(3);
+//         $page->screenshot([
+//             'captureBeyondViewport' => true,
+//             'clip' => $page->getFullPageClip()
+//         ])->saveToFile(__DIR__ . "/screenshots/bar{$i}.png");
+//     }
+// } finally {
+//     // bye
+//     $browser->close();
+// }
 
+$mpdf = new Mpdf([ // set page orientation to landscape (default is portrait)
+    'orientation' => 'L'
+]);
 
-    // pdf
-    // $page->pdf(['printBackground' => false])->saveToFile(__DIR__ .' bar.pdf');
-} finally {
-    // bye
-    $browser->close();
-}
+$mpdf->WriteHTML('<h1>Screen1</h1><img src="' . __DIR__ . "/screenshots/bar1.png" . '"/>');
+$mpdf->AddPage('P'); // set this page to portrait
+$mpdf->WriteHTML('$<h1>Screen2</h1><img src="' . __DIR__ . "/screenshots/bar1.png" . '"/>');
+$mpdf->AddPage('L'); // set this page to landscape
+$mpdf->WriteHTML('$<h1>Screen2</h1><img src="' . __DIR__ . "/screenshots/bar2.png" . '"/>');
 
-// echo __DIR__;
+// Output the PDF to the browser for inline viewing or force a download
+// 'I' sends the file inline to the browser (default)
+// 'D' sends to the browser and forces a file download
+$mpdf->Output('my_document.pdf', \Mpdf\Output\Destination::INLINE);
