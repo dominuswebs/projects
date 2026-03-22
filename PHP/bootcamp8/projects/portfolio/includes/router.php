4617b6c3d06b27825792a703e0b9237b4bec2ff2
Hugo Paz <dominuswebs@gmail.com>
2026-03-23 08:32:56 +1100

    bootcamp code

--- PHP/bootcamp8/projects/portfolio/includes/router.php
+++ PHP/bootcamp8/projects/portfolio/includes/router.php
@@ -14,7 +14,7 @@ function normaliseUri(string $uri): string {
 
 function notFound(): void {
     http_response_code(404);
-    echo "404 Not Found";
+    echo "404 Not Found Custom";
     // this stops the script execution
     exit;
 }
